fb9a3840a2239c368a29d0ec81a6f374b2a3785e
HYEBA <aslk3366zz@naver.com>
2021-03-17 18:51:52 +0900

    새 액티비티 추가

---
+++ app/src/main/java/org/jangtechtown/study_20210317/MyFirstActivity.kt
@@ -0,0 +1,11 @@
+package org.jangtechtown.study_20210317
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyFirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_first)
+    }
+}
